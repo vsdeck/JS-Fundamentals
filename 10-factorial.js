62730c4f01eebe83b4ae641f84f3c2d7299979d4
Franklin <vsdeckx@gmail.com>
2025-09-13 19:11:57 +0000

    Add 10-factorial.js script to recursively compute and print factorial of an integer or 1 for NaN

---
+++ 10-factorial.js
@@ -0,0 +1,9 @@
+function factorial(n) {
+    if (isNaN(n) || n <= 1) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+const arg = parseInt(process.argv[2], 10);
+console.log(factorial(arg));
