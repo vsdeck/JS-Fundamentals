b8254c952f0b9967d32290c2f94a225e282ebaf9
Franklin <vsdeckx@gmail.com>
2025-09-13 18:48:33 +0000

    Update 7-multi_c.js script to ignore negative numbers and show error only for invalid input

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,9 +1,9 @@
 const arg = process.argv[2];
 const count = parseInt(arg, 10);
 
-if (isNaN(count) || count < 1) {
+if (isNaN(count)) {
     console.log("Missing number of occurrences");
-} else {
+} else if (count >= 0) {
     for (let i = 0; i < count; i++) {
         console.log("C is fun");
     }
